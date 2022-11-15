5e90a17e013f353280c2bab99c611a590eb39466
abdallah.issa <abdallah.issa@gmail.com>
2022-11-15 07:19:02 +0200

    added final main after testing the whole project

--- main.c
+++ main.c
@@ -3,19 +3,21 @@
 #include "LIB/BIT_MATH.h"
 #include <util/delay.h>
 
-/*********************** MCAL libraries ***********************/
+/*********************** MCAL Includes ***********************/
 #include "MCAL/GIE/GIE_interface.h"
 #include "MCAL/GPIO/GPIO_interface.h"
 #include "MCAL/EXTI/EXTI_interface.h"
 #include "MCAL/TIMER/TIMERS_interface.h"
 #include "MCAL/ADC/ADC_interface.h"
+#include "MCAL/I2C/I2C_interface.h"
 
-/************************ HAL libraries ***********************/
+/************************ HAL Includes ***********************/
 #include "HAL/KEYPAD/KEYPAD_interface.h"
-#include "HAL/LCD/LCD_interface.h"
+/* Bye Bye Normal LCD , Welcome LCD_I2C */
+#include "HAL/LCD_I2C/LCD_I2C_interface.h"
 #include "HAL/SERVOM/SERVOM_interface.h"
 
-/*********************** APP libraries ************************/
+/*********************** APP Includes ************************/
 #include "App/APP_interface.h"
 
 /*********************** main Function ************************/
@@ -24,12 +26,15 @@ int main(void)
 	/*************** peripheral initializations ***************/
 	APP_vInit();
 
+	// welcome message on LCD at the start of the system
 	APP_vWelcomeScreen();
+
 	/********************** infinite loop *********************/
 	while(1)
 	{
 		APP_vStartTheSystem();
 	}
+
 	return 0;
 }
 
